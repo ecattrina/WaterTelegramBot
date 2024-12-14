00931bc3c198a0f86a9d05ac87966c7fa5604d32
ecattrina <katy_060104@mail.ru>
2024-12-14 17:47:54 +0500

    Создание проекта

---
+++ WaterTgBot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WaterTgBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
